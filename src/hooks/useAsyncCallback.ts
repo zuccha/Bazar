133a57b2238169665ef074186c3b18cebda7e1a7
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2022-01-01 23:49:30 +0100

    feat: export type for async callback

--- src/hooks/useAsyncCallback.ts
+++ src/hooks/useAsyncCallback.ts
@@ -2,14 +2,16 @@ import { DependencyList, useCallback } from 'react';
 import { ErrorReport } from '../utils/ErrorReport';
 import useSafeState from './usSafeState';
 
-export default function useAsyncCallback<Args extends unknown[]>(
-  callback: (...args: Args) => Promise<ErrorReport | undefined>,
-  deps: DependencyList,
-): {
+export interface AsyncCallback<Args extends unknown[]> {
   call: (...args: Args) => Promise<ErrorReport | undefined>;
   error: ErrorReport | undefined;
   isLoading: boolean;
-} {
+}
+
+export default function useAsyncCallback<Args extends unknown[]>(
+  callback: (...args: Args) => Promise<ErrorReport | undefined>,
+  deps: DependencyList,
+): AsyncCallback<Args> {
   const [error, setError] = useSafeState<ErrorReport | undefined>(undefined);
   const [isLoading, setIsLoading] = useSafeState(false);
 
