aed9542fddcb25e5977f05e71b7fa5a55600c930
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2022-01-02 02:36:21 +0100

    feat: handle startup errors

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,10 @@ import { useCore } from './contexts/CoreContext';
 import { useSettings, useToolchain } from './core-hooks/Core';
 import { useLoadSettings } from './core-hooks/Settings';
 import { useLoadToolchain } from './core-hooks/Toolchain';
+import useHandleError from './hooks/useHandleError';
 import Navigator from './ui/Navigator';
+
+const errorTitles = ['Failed to load settings', 'Failed to load tools'];
 
 export default function App() {
   const [isLoading, setIsLoading] = useState(true);
@@ -16,9 +19,14 @@ export default function App() {
   const toolchain = useToolchain();
   const loadToolchain = useLoadToolchain(toolchain);
 
+  const handleError = useHandleError();
+
   useEffect(() => {
-    Promise.all([loadSettings(), loadToolchain()]).then((responses) => {
-      setIsLoading(false);
+    Promise.all([loadSettings(), loadToolchain()]).then((maybeErrors) => {
+      maybeErrors.forEach((maybeError, index) =>
+        handleError(maybeError, errorTitles[index] || 'Failed to load'),
+      ),
+        setIsLoading(false);
     });
   }, [loadSettings, loadToolchain]);
 
