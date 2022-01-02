7d636ee1992008406ce14cd24af41c3ae49e4017
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2022-01-02 01:46:47 +0100

    feat: create select component

---
+++ src/ui-atoms/input/Select.tsx
@@ -0,0 +1,64 @@
+import * as Chakra from '@chakra-ui/react';
+import { LayoutProps, SpaceProps } from '@chakra-ui/react';
+import { ChangeEvent, forwardRef, ReactElement, Ref, useCallback } from 'react';
+import useColorScheme from '../../theme/useColorScheme';
+
+interface SelectProps extends LayoutProps, SpaceProps {
+  isDisabled?: boolean;
+  isFullWidth?: boolean;
+  onChange: (value: string) => void;
+  options: {
+    value: string;
+    label: string;
+  }[];
+  placeholder: string;
+  value: string;
+}
+
+function Select(
+  {
+    isDisabled,
+    isFullWidth = false,
+    onChange,
+    options,
+    placeholder,
+    value,
+    ...props
+  }: SelectProps,
+  ref: Ref<HTMLSelectElement>,
+): ReactElement {
+  const colorScheme = useColorScheme();
+
+  const handleChange = useCallback(
+    (event: ChangeEvent<HTMLSelectElement>) => {
+      onChange(event.target.value);
+    },
+    [onChange],
+  );
+
+  return (
+    <Chakra.Select
+      bg='transparent'
+      borderColor={`${colorScheme}.600`}
+      borderRadius='0'
+      colorScheme={colorScheme}
+      disabled={isDisabled}
+      isFullWidth={isFullWidth}
+      onChange={handleChange}
+      py={1}
+      ref={ref}
+      size='sm'
+      value={value}
+      variant='outline'
+      {...props}
+    >
+      {options.map((option) => (
+        <option key={option.value} value={option.value}>
+          {option.label}
+        </option>
+      ))}
+    </Chakra.Select>
+  );
+}
+
+export default forwardRef(Select);
