3f2caa81957677528482563c3bfb2e6dddb66596
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2022-01-04 20:39:00 +0100

    fix: prevent SelectorOfFiles to rerender infinitely

--- src/ui-atoms/SelectorOfFiles.tsx
+++ src/ui-atoms/SelectorOfFiles.tsx
@@ -37,7 +37,7 @@ export default function SelectorOfFiles({
       setOptions(newOptions);
     }
     return undefined;
-  }, [directoryPath, extensions]);
+  }, [directoryPath, ...extensions]);
 
   useEffect(() => {
     computeOptions.call();
