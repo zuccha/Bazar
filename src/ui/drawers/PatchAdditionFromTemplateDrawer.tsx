87fcb8ffb5cfdfb834f3b0fca119e6c94d84cf28
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2022-01-08 20:46:14 +0100

    feat: add patch info editor in patch addition from template drawer (it does nothing)

--- src/ui/drawers/PatchAdditionFromTemplateDrawer.tsx
+++ src/ui/drawers/PatchAdditionFromTemplateDrawer.tsx
@@ -1,15 +1,21 @@
-import { Flex } from '@chakra-ui/react';
-import { ReactElement } from 'react';
-import { useCollectionPatchNames } from '../../core-hooks/Collection';
+import { VStack } from '@chakra-ui/react';
+import { ReactElement, useEffect, useState } from 'react';
+import {
+  useCollectionPatch,
+  useCollectionPatchNames,
+} from '../../core-hooks/Collection';
 import { useCollection } from '../../core-hooks/Core';
+import Patch from '../../core/Patch';
 import { useList } from '../../hooks/useAccessors';
 import useForm from '../../hooks/useForm';
 import useFormField from '../../hooks/useFormField';
+import useToast from '../../hooks/useToast';
 import Button from '../../ui-atoms/Button';
 import Drawer from '../../ui-atoms/Drawer';
 import FormControl from '../../ui-atoms/FormControl';
 import Selector from '../../ui-atoms/Selector';
 import ErrorReport from '../../utils/ErrorReport';
+import { PatchInfoFields, usePatchInfoFields } from '../forms/PatchInfoForm';
 
 interface PatchAdditionDrawerProps {
   onClose: () => void;
@@ -20,15 +26,25 @@ export default function PatchAdditionFromTemplateDrawer({
   onClose,
   onAdd,
 }: PatchAdditionDrawerProps): ReactElement {
+  const toast = useToast();
+
   const collection = useCollection();
   const patchNames = useCollectionPatchNames(collection);
+
+  const [patchName, setPatchName] = useState(patchNames[0] ?? '');
+  const patch = useCollectionPatch(collection, patchName);
 
   const templateNameField = useFormField({
     infoMessage: 'Name of the patch template',
     initialValue: patchNames[0] || '',
     isRequired: true,
     label: 'Patch template',
+    onChange: setPatchName,
   });
+
+  const infoFields = usePatchInfoFields(
+    patch.value?.getInfo() ?? Patch.InfoEmpty,
+  );
 
   const form = useForm({
     fields: [templateNameField],
@@ -40,19 +56,37 @@ export default function PatchAdditionFromTemplateDrawer({
     value: patchName,
   }));
 
+  useEffect(() => {
+    infoFields.nameField.handleChange(patch.value?.getInfo().name ?? '');
+    infoFields.versionField.handleChange(patch.value?.getInfo().version ?? '');
+    infoFields.authorField.handleChange(patch.value?.getInfo().author ?? '');
+    infoFields.mainFileRelativePathField.handleChange(
+      patch.value?.getInfo().mainFileRelativePath ?? '',
+    );
+  }, [patch.value]);
+
+  useEffect(() => {
+    if (patch.error) {
+      toast.failure(
+        'Failed to find valid info for the selected template',
+        patch.error,
+      );
+    }
+  }, [patch.error]);
+
   return (
     <Drawer
       buttons={
         <>
           <Button
-            isDisabled={form.isSubmitting}
+            isDisabled={form.isSubmitting || patch.isLoading}
             label='Cancel'
             onClick={onClose}
             variant='outline'
             mr={3}
           />
           <Button
-            isDisabled={!form.isValid || form.isSubmitting}
+            isDisabled={!form.isValid || form.isSubmitting || patch.isLoading}
             label='Create'
             onClick={async () => {
               const maybeError = await form.handleSubmit();
@@ -66,7 +100,7 @@ export default function PatchAdditionFromTemplateDrawer({
       onClose={onClose}
       title='Add patch from template'
     >
-      <Flex direction='column' flex={1}>
+      <VStack flex={1} w='100%' spacing={2} alignItems='stretch'>
         <FormControl {...templateNameField.control}>
           <Selector
             isDisabled={form.isSubmitting}
@@ -76,7 +110,12 @@ export default function PatchAdditionFromTemplateDrawer({
             value={templateNameField.value}
           />
         </FormControl>
-      </Flex>
+        <PatchInfoFields
+          directoryPath={patch.value?.getPath() ?? ''}
+          isTurnedOff={patch.isLoading}
+          {...infoFields}
+        />
+      </VStack>
     </Drawer>
   );
 }
