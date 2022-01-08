a7ad8483503ef2fd0eb8de89f84f8a9a7bdd773f
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2022-01-08 14:39:15 +0100

    refactor: remove unused map method from EitherErrorOr

--- src/utils/EitherErrorOr.ts
+++ src/utils/EitherErrorOr.ts
@@ -4,20 +4,12 @@ export interface EitherError {
   readonly isError: true;
   readonly isValue: false;
   readonly error: ErrorReport;
-  readonly map: <R>(
-    callbackError: (error: ErrorReport) => R,
-    callbackValue: unknown,
-  ) => R;
 }
 
 export interface EitherValue<T> {
   readonly isError: false;
   readonly isValue: true;
   readonly value: T;
-  readonly map: <R>(
-    callbackError: unknown,
-    callbackRight: (value: T) => R,
-  ) => R;
 }
 
 export type EitherErrorOr<T> = EitherError | EitherValue<T>;
@@ -27,13 +19,11 @@ export const $EitherErrorOr = {
     isError: true,
     isValue: false,
     error,
-    map: (callback) => callback(error),
   }),
 
   value: <T>(value: T): EitherValue<T> => ({
     isError: false,
     isValue: true,
     value,
-    map: (_, callback) => callback(value),
   }),
 };
